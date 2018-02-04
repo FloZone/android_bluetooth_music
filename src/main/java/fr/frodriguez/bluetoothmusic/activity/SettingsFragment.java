33bce7954cd82056f2049d1cb19ae3e0408d558d
FloZone <flozone45@hotmail.fr>
2018-02-04 16:43:38 +0100

    New icon

--- src/main/java/fr/frodriguez/bluetoothmusic/activity/SettingsFragment.java
+++ src/main/java/fr/frodriguez/bluetoothmusic/activity/SettingsFragment.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.Preference;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
